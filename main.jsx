bfcf19c65a3f4c62432592d34783b3e67762a686
fq7srgzj6d-design <fq7srgzj6d@privaterelay.appleid.com>
2025-11-24 20:51:51 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LuminaryX from './LuminaryX.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LuminaryX />
+  </React.StrictMode>
+);
