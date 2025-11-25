8c62324f643e7ce91088c0ee2e1cba51e2a7ba57
fq7srgzj6d-design <fq7srgzj6d@privaterelay.appleid.com>
2025-11-24 22:52:59 -0600

    Reorder import statements in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import LuminaryX from './LuminaryX.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import LuminaryX from './LuminaryX.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LuminaryX />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
